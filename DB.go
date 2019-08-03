ee22acc56bb6fd4b3677393687cbb03b72b7cb52
yuanxs <yuanxs@95szzl.cn>
2019-08-03 14:58:23 +0800

    201908031454

--- DB.go
+++ DB.go
@@ -3,6 +3,7 @@ package goToolMSSqlHelper
 import (
 	"errors"
 	"github.com/Deansquirrel/goToolMSSql"
+	"github.com/Deansquirrel/goToolMSSql2000"
 )
 
 const (
@@ -12,6 +13,30 @@ const (
 
 func GetDbId(dbConfig *goToolMSSql.MSSqlConfig) (int, error) {
 	rows, err := GetRowsBySQL(dbConfig, sqlGetDbId)
+	if err != nil {
+		return -1, err
+	}
+	var id int
+	flag := false
+	for rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			return -1, err
+		}
+		flag = true
+	}
+	if rows.Err() != nil {
+		return -1, rows.Err()
+	}
+	if flag {
+		return id, nil
+	} else {
+		return -1, errors.New("get db id return empty")
+	}
+}
+
+func GetDbId2000(dbConfig *goToolMSSql2000.MSSqlConfig) (int, error) {
+	rows, err := GetRowsBySQL2000(dbConfig, sqlGetDbId)
 	if err != nil {
 		return -1, err
 	}
